6ca120949cbbb0aea4c936245b878550a0bae672
IMRAN5377 <115469598+IMRAN5377@users.noreply.github.com>
2022-10-27 15:52:27 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,9 +6,3 @@ def newMaven()
 {
   sh 'mvn package'
 }
-def deploy(jobname,ip,context)
-{
-  def run selinium(jobname)
-  {
-    sh "scp|war|lib|workspace/${jobname}/webapp/target/webapp.war ubuntu@ ${ip} |var|lib|tomcat9|webapp/{${context}."war"
-  }
